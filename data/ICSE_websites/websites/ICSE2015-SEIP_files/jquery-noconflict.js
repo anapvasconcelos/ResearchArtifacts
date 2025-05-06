db7417a31ffb36aa6d70a0adffbfeadf30ca5f69
Ana Paula Vasconcelos <78928664+anapvasconcelos@users.noreply.github.com>
2025-05-06 18:36:38 -0400

    Add files via upload

---
+++ data/ICSE_websites/websites/ICSE2015-SEIP_files/jquery-noconflict.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+jQuery.noConflict();
+
+
+}
+/*
+     FILE ARCHIVED ON 00:21:07 Mar 27, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:21:54 Mar 19, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.593
+  exclusion.robots: 0.02
+  exclusion.robots.policy: 0.008
+  esindex: 0.012
+  cdx.remote: 76.504
+  LoadShardBlock: 284.451 (3)
+  PetaboxLoader3.datanode: 169.199 (5)
+  PetaboxLoader3.resolve: 766.051 (3)
+  load_resource: 715.744 (2)
+*/
