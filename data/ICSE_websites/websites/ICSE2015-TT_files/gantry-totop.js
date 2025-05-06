de8642909f679ab30a2c13216caf2e862de1348e
Ana Paula Vasconcelos <78928664+anapvasconcelos@users.noreply.github.com>
2025-05-06 18:36:45 -0400

    Add files via upload

---
+++ data/ICSE_websites/websites/ICSE2015-TT_files/gantry-totop.js
@@ -0,0 +1,41 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/*
+ * @author    RocketTheme http://www.rockettheme.com
+ * @copyright Copyright (C) 2007 - 2013 RocketTheme, LLC
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ */
+window.addEvent("domready",function(){var b=document.id("gantry-totop");if(b){var a=new Fx.Scroll(window);b.setStyle("outline","none").addEvent("click",function(c){c.stop();
+a.toTop();});}});
+
+}
+/*
+     FILE ARCHIVED ON 00:21:44 Mar 27, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:21:56 Mar 19, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.573
+  exclusion.robots: 0.017
+  exclusion.robots.policy: 0.008
+  esindex: 0.01
+  cdx.remote: 16.859
+  LoadShardBlock: 201.446 (3)
+  PetaboxLoader3.datanode: 142.084 (5)
+  PetaboxLoader3.resolve: 412.26 (3)
+  load_resource: 406.123 (2)
+*/
