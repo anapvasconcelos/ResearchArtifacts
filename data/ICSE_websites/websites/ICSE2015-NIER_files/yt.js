0c314b5faf1cc6cc44bd88bf7f834921f33dc787
Ana Paula Vasconcelos <78928664+anapvasconcelos@users.noreply.github.com>
2025-05-06 18:33:38 -0400

    Add files via upload

---
+++ data/ICSE_websites/websites/ICSE2015-NIER_files/yt.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+var tag=document.createElement('script');tag.src="https://web.archive.org/web/20160327002320/http://www.youtube.com/player_api";var firstScriptTag=document.getElementsByTagName('script')[0];firstScriptTag.parentNode.insertBefore(tag,firstScriptTag);var player;function onYouTubePlayerAPIReady(){player=new YT.Player('player',{height:'360',width:'100%',videoId:'S0_zfTN5_jI'})}function playerSeekTo(player,seconds){player.seekTo(seconds)}$(window).load(function(){jQuery('#flight').click(function(){console.log(player);player.seekTo(101);return false})});$(window).load(function(){jQuery('#train').click(function(){console.log(player);player.seekTo(162);return false})});$(window).load(function(){jQuery('#weather').click(function(){console.log(player);player.seekTo(189);return false})});$(window).load(function(){jQuery('#fastvisit').click(function(){console.log(player);player.seekTo(224);return false})});$(window).load(function(){jQuery('#aroundflorence').click(function(){console.log(player);player.seekTo(235);return false})});$(window).load(function(){jQuery('#tour').click(function(){console.log(player);player.seekTo(288);return false})});
+
+
+}
+/*
+     FILE ARCHIVED ON 00:23:20 Mar 27, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:22:00 Mar 19, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.762
+  exclusion.robots: 0.033
+  exclusion.robots.policy: 0.015
+  esindex: 0.014
+  cdx.remote: 16.515
+  LoadShardBlock: 131.117 (3)
+  PetaboxLoader3.datanode: 171.682 (5)
+  load_resource: 386.243 (2)
+  PetaboxLoader3.resolve: 301.341 (2)
+*/
